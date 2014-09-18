91940c96545cfb6d4d129e3a4f9555801b78e27f
personary <apersonary@gmail.com>
2014-09-17 21:54:04 -0400

    added a test class

---
+++ JHTest.h
@@ -0,0 +1,13 @@
+//
+//  JHTest.h
+//  Git Test
+//
+//  Created by Joshua Haines on 9/17/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JHTest : NSObject
+
+@end
---
+++ JHTest.m
@@ -0,0 +1,13 @@
+//
+//  JHTest.m
+//  Git Test
+//
+//  Created by Joshua Haines on 9/17/14.
+//
+//
+
+#import "JHTest.h"
+
+@implementation JHTest
+
+@end
